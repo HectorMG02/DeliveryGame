145b332ecc288d1268fc0e921d9be17c692e643a
hector.matias@equipoiriego.es <hector.matias@equipoiriego.es>
2022-01-12 15:44:51 +0100

    movimiento del coche

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] private float steerSpeed = 1f;
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+
+        transform.Rotate(0, 0, -steerAmount);
+        
+        transform.Translate(0, moveAmount, 0);
+    }
+}
+
+
+ 
